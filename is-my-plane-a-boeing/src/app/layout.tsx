c8fce682616742992c49f0bd8bef7684f259fc9a
Ryan Huang <59319094+ryan-huang1@users.noreply.github.com>
2024-08-29 15:35:11 -0400

    Update layout.tsx

--- is-my-plane-a-boeing/src/app/layout.tsx
+++ is-my-plane-a-boeing/src/app/layout.tsx
@@ -17,15 +17,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head>
+      <body className={inter.className}>
+        {children}
         <Script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9715108521528095"
           crossOrigin="anonymous"
           strategy="afterInteractive"
         />
-      </head>
-      <body className={inter.className}>{children}</body>
+      </body>
     </html>
   )
 }
