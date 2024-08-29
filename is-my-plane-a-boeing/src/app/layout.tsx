109b89a85ab4d6facbe7535c447dbc6f0bb284e0
Ryan Huang <59319094+ryan-huang1@users.noreply.github.com>
2024-08-29 15:34:36 -0400

    add google adsense tag

--- is-my-plane-a-boeing/src/app/layout.tsx
+++ is-my-plane-a-boeing/src/app/layout.tsx
@@ -1,25 +1,31 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+import Script from 'next/script'
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: "Boeing or Not",
-  description: "If it's Boeing, I'm not going",
-  icons: {
-    icon: '/favicon.ico', // Add this line to include the favicon
-  },
-};
+  title: 'Is My Plane a Boeing',
+  description: 'Check if your plane is a Boeing',
+}
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: {
+  children: React.ReactNode
+}) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9715108521528095"
+          crossOrigin="anonymous"
+          strategy="afterInteractive"
+        />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
-  );
+  )
 }
